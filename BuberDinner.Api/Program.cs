1bd71f8cd5713fa5aee864e9d41182bd38d3790a
MTomys <michalltomys@protonmail.ch>
2023-02-18 21:08:24 +0100

    add mappings

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,12 +1,16 @@
+using BuberDinner.Api;
 using BuberDinner.Api.Common.Errors;
+using BuberDinner.Api.Common.Mappings;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
-    builder.Services.AddApplication();
-    builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services
+        .AddPresentation()
+        .AddApplication()
+        .AddInfrastructure(builder.Configuration);
     builder.Services.AddControllers();
     builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
